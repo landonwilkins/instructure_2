cf1de28d66bad0cac561dfa2e0ec493e413c0dbb
Landon Wilkins <landonwilkins@gmail.com>
2013-04-22 13:57:50 -0600

    Initial scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 WimpyBlog::Application.routes.draw do
+  resources :comments
+
+
+  resources :users
+
+
+  resources :posts
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
