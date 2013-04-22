73f53a77dd0cf5031f4650bd7ae28c80e896f89b
Landon Wilkins <landonwilkins@gmail.com>
2013-04-22 13:48:40 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WimpyBlog::Application.config.secret_token = '033178c6498144ab26eea4b11ddd6acf258107ae727451fbe12a64b9ea04326faf3088a615ca2649e46c13b0a740ba368f7d77bce4a448a5d16df37e4588d84d'
